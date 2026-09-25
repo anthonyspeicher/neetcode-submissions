d3ec4ca1c5db826232830576c846fb3ac6112957
Anthony Speicher <anthony@speicher.org>
2026-09-24 21:19:52 -0400

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        return binary_search(0, nums.size() - 1, nums, target);
+    }
+
+private:
+    int binary_search(int l, int r, vector<int>& nums, int target){
+        if (l > r) return -1;
+        int m = l + (r - l) / 2;
+
+        if (nums[m] == target)
+            return m;
+        else if (nums[m] < target) {
+            return binary_search(m + 1, r, nums, target);
+        } else return binary_search(l, m - 1, nums, target);
+    }
+};
